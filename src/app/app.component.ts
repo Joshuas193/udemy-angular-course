a8e8126ad7abe193a05c3be4abc585b187f7a6c7
Joshua Swartzell <joshuas193@live.com>
2021-05-18 14:33:42 -0500

    complete step 25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+      margin: 8px 0;
+    }
+  `]
 })
 export class AppComponent {
   name = 'Josh';
