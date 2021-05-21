2ef27a9131a7fd4c4b06be81ea203d0645fdd78e
Joshua Swartzell <joshuas193@live.com>
2021-05-21 14:30:15 -0500

    step 31

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No Server was Created";
 
   constructor() {
     setTimeout(() => {
@@ -15,6 +16,14 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was Created";
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
   }
 
 }
